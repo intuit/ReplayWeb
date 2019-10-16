7af66c76bdba2ee0cf4b5f57ed379757008ca8d3
Shashank Srigiri <Shashank_Srigiri@intuit.com>
2019-10-17 00:03:18 +0530

    recommended changes

--- packages/utils/test/index.test.js
+++ packages/utils/test/index.test.js
@@ -613,7 +613,7 @@ describe('Regex Replace', () => {
     expect(regExpMatch('/b\\w+/', 'foobar')).toEqual('bar')
   })
   it('should find a word that starts with one or more word characters, has "@" and ends with one or more word characters', () => {
-    expect(regExpMatch('/\\w+@\\w+/', ' any string such as abc@gmail ')).toEqual('abc@gmail')
+    expect(regExpMatch('/\\w+@\\w+/', ' any string such as abc@gmail  ')).toEqual('abc@gmail')
   })
 })
 
