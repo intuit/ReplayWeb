a0b23f22e229f127cc5973bfe512202505acf90a
Chow, Kevin <Kevin_Chow@intuit.com>
2019-10-21 10:39:53 -0600

    Add unit tests for RenameModal

--- packages/extension/test/components/Modals/RenameModal.test.js
+++ packages/extension/test/components/Modals/RenameModal.test.js
@@ -1,6 +1,18 @@
 import React from 'react'
-import { cleanup, render } from '@testing-library/react'
+import { cleanup, render, fireEvent, act } from '@testing-library/react'
 import RenameModal from '../../../src/components/Modals/RenameModal.jsx'
+import * as C from '../../../src/common/constant'
+import { message } from 'antd'
+
+jest.mock('antd', () => {
+  return {
+    ...(jest.requireActual('antd')),
+    message: {
+      success: jest.fn(),
+      error: jest.fn()
+    }
+  }
+})
 
 afterEach(() => {
   cleanup()
@@ -30,5 +42,84 @@ describe('RenameModal', () => {
     getByText('Save')
   })
 
-  // TODO more tests ...
+  it('should change rename on input change', () => {
+    const { getByPlaceholderText } = render(getComponent())
+    const input = getByPlaceholderText('test case name')
+    fireEvent.change(input, { target: { value: 'new name' }})
+    expect(input.value).toEqual('new name')
+  })
+
+  it('should rename test case on modal OK', async () => {
+    const mockProps = {
+      editorStatus: C.EDITOR_STATUS.TESTS,
+      renameTestCase: jest.fn().mockResolvedValue(),
+      closeModal: jest.fn()
+    }
+    const { getByText } = render(getComponent(mockProps))
+    const modalOk = getByText('Save')
+    await act(async () => {
+      fireEvent.click(modalOk)
+    })
+    expect(mockProps.renameTestCase).toHaveBeenCalled()
+    expect(mockProps.closeModal).toHaveBeenCalled()
+  })
+
+  it('should rename block on modal OK', async () => {
+    const mockProps = {
+      editorStatus: C.EDITOR_STATUS.BLOCKS,
+      renameBlock: jest.fn().mockResolvedValue(),
+      closeModal: jest.fn()
+    }
+    const { getByText } = render(getComponent(mockProps))
+    const modalOk = getByText('Save')
+    await act(async () => {
+      fireEvent.click(modalOk)
+    })
+    expect(mockProps.renameBlock).toHaveBeenCalled()
+    expect(mockProps.closeModal).toHaveBeenCalled()
+  })
+
+  it('should rename suites on modal OK', async () => {
+    const mockProps = {
+      editorStatus: C.EDITOR_STATUS.SUITES,
+      renameSuite: jest.fn().mockResolvedValue(),
+      closeModal: jest.fn()
+    }
+    const { getByText } = render(getComponent(mockProps))
+    const modalOk = getByText('Save')
+    await act(async () => {
+      fireEvent.click(modalOk)
+    })
+    expect(mockProps.renameSuite).toHaveBeenCalled()
+    expect(mockProps.closeModal).toHaveBeenCalled()
+  })
+
+  it('should catch error on modal OK', async () => {
+    const errorMessage = 'error msg'
+    const mockProps = {
+      editorStatus: C.EDITOR_STATUS.SUITES,
+      renameSuite: jest.fn().mockRejectedValue({message: errorMessage}),
+      closeModal: jest.fn()
+    }
+    const { getByText } = render(getComponent(mockProps))
+    const modalOk = getByText('Save')
+    await act(async () => {
+      fireEvent.click(modalOk)
+    })
+    expect(message.error).toHaveBeenCalledWith(errorMessage, 1.5)
+  })
+  
+  it('should call onCancel on modal cancel', async () => {
+    const mockProps = {
+      closeModal: jest.fn()
+    }
+    const { getByPlaceholderText, getByText } = render(getComponent(mockProps))
+    const input = getByPlaceholderText('test case name')
+    const modalCancel = getByText('Cancel')
+    await act(async () => {
+      fireEvent.click(modalCancel)
+    })
+    expect(mockProps.closeModal).toHaveBeenCalled()
+    expect(input.value).toEqual('')
+  })
 })
