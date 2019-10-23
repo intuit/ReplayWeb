c35e40f4612955ffd44800a02248dc3ebf664678
ANIKET PAUL <aniketpaul1592@gmail.com>
2019-10-23 21:17:56 +0530

    Adding new test cases

--- packages/extension/test/components/Modals/DuplicateModal.test.js
+++ packages/extension/test/components/Modals/DuplicateModal.test.js
@@ -22,10 +22,56 @@ const getComponent = (props = {}) => {
 }
 
 describe('DuplicateModal', () => {
-  it('renders', () => {
+  it('does not render', () => {
     const { container } = render(getComponent())
     expect(container.querySelector('input')).toBeNull()
   })
-
-  // TODO more tests ...
+  it('renders when visible is true', () => {
+    const wrapper = shallow(getComponent({visible: true}))
+    expect(wrapper.find('Modal')).not.toBeNull()
+  })
+  it('test onCancel', () => {
+    const closeModalSpy = jest.fn()
+    const wrapper = shallow(getComponent({
+      visible: true,
+      closeModal: closeModalSpy
+    }))
+    expect(wrapper.find('Modal')).not.toBeNull()
+    wrapper.instance().onCancel()
+    expect(wrapper.state("duplicateName")).toEqual("")
+    expect(closeModalSpy).toHaveBeenCalled()
+  })
+  it('test onCancel', () => {
+    const closeModalSpy = jest.fn()
+    const wrapper = shallow(getComponent({
+      visible: true,
+      closeModal: closeModalSpy
+    }))
+    expect(wrapper.find('Modal')).not.toBeNull()
+    wrapper.instance().onCancel()
+    expect(wrapper.state("duplicateName")).toEqual("")
+    expect(closeModalSpy).toHaveBeenCalled()
+  })
+  it('test onChange', () => {
+    const wrapper = shallow(getComponent({
+      visible: true
+    }))
+    const e = { target: { value: "test"}}
+    expect(wrapper.find('Modal')).not.toBeNull()
+    wrapper.instance().onChange(e)
+    expect(wrapper.state("duplicateName")).toEqual("test")
+  })
+  it('test onDuplicate resolves successfully', () => {
+    const closeModalSpy = jest.fn()
+    const duplicateMock = jest.fn()
+    const wrapper = shallow(getComponent({
+      visible: true,
+      closeModal: closeModalSpy,
+      duplicate: duplicateMock
+    }))
+    expect(wrapper.find('Modal')).not.toBeNull()
+    wrapper.instance().onDuplicate()
+    expect(wrapper.state("duplicateName")).toEqual("")
+    expect(closeModalSpy).toHaveBeenCalled()
+  })
 })
