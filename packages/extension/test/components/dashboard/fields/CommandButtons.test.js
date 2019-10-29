ac60c7140473d117dceaa7ce5ebe15e2ed364516
Noon Lyle <nhung_lyle@intuit.com>
2019-10-29 13:50:03 -0700

    First jsx unit tests

--- packages/extension/test/components/dashboard/fields/CommandButtons.test.js
+++ packages/extension/test/components/dashboard/fields/CommandButtons.test.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import { cleanup, render } from '@testing-library/react'
+import { cleanup, render, fireEvent } from '@testing-library/react'
 import CommandButtons from '../../../../src/components/dashboard/fields/CommandButtons.jsx'
+import ipc from '../../../../src/common/ipc/ipc_cs'
 
-jest.mock('../../../../src/common/ipc/ipc_cs', () => {})
+jest.mock('../../../../src/common/ipc/ipc_bg_cs', () => ({
+  csInit: () => ({
+    ask: jest.fn().mockReturnValue(Promise.resolve())
+  })
+}))
 
 afterEach(() => {
   cleanup()
@@ -35,5 +40,46 @@ describe('CommandButtons', () => {
     expect(container.querySelector('.ant-btn-group')).not.toBeNull()
   })
 
-  // TODO more tests ...
+  it('test onClickFindButton is not undefined and clickable', () => {
+    const { queryByTestId } = render(getComponent({
+      canTarget: true,
+      isCmdEditable: true
+    }))
+    const button = queryByTestId('onClickFindButton')
+    expect(button).not.toBeUndefined()
+    fireEvent.click(button)
+    expect(ipc.ask).toHaveBeenCalledTimes(1)
+    expect(ipc.ask).toHaveBeenCalledWith("PANEL_HIGHLIGHT_DOM", {"lastOperation": null, "locator": ""})
+  })
+
+  it('test toggleInspectButton inspecting true', () => {
+    const mockSetInspectingTarget = jest.fn()
+    const mockStopInspecting = jest.fn()
+    const { queryByTestId } = render(getComponent({
+      canTarget: true,
+      isInspecting: true,
+      setInspectTarget: mockSetInspectingTarget,
+      stopInspecting: mockStopInspecting,
+      isCmdEditable: true
+    }))
+    const toggleButton = queryByTestId('toggleIns)pectButton')
+    fireEvent.click(toggleButton)
+    expect(mockSetInspectingTarget).toHaveBeenCalledTimes(1)
+    expect(mockStopInspecting).toHaveBeenCalledTimes(1)
+  })
+
+  it('test toggleInspectButton inspecting false', () => {
+    const mockSetInspectingTarget = jest.fn()
+    const mockStartInspecting = jest.fn()
+    const { queryByTestId } = render(getComponent({
+      canTarget: true,
+      setInspectTarget: mockSetInspectingTarget,
+      startInspecting: mockStartInspecting,
+      isCmdEditable: true
+    }))
+    const toggleButton = queryByTestId('toggleInspectButton')
+    fireEvent.click(toggleButton)
+    expect(mockSetInspectingTarget).toHaveBeenCalledTimes(1)
+    expect(mockStartInspecting).toHaveBeenCalledTimes(1)
+  })
 })
